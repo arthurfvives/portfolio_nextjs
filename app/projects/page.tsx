347b9d3ccd6526e9d3e1aca3b1782c113153e7af
arthurfvives <arthur.ferket@student.vives.be>
2025-08-29 12:36:39 +0200

    small navbar improvement

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,4 @@
 import { Project } from "@/types/Project";
-import ProjectCard from "@/components/projectImage";
 import ProjectImage from "@/components/projectImage";
 
 export default async function Page() {
@@ -12,7 +11,6 @@ export default async function Page() {
 
   return (
     <div>
-      <h1 className="text-3xl font-bold mb-8">Project Page</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
         {projects.map((project: Project) => (
           <div key={project.id} className="text-center">
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 
 const Navbar = () => (
-  <nav className="flex justify-between items-center px-4 py-6 bg-transparent">
+  <nav className="flex justify-between items-center px-4 py-6 bg-transparent mx-10">
     <div className="font-bold text-xl">
       <Link href="/">Merel Vandekerkhove</Link>
     </div>
     <div className="flex gap-6">
+      <Link href="/">Home</Link>
       <Link href="/projects">Projects</Link>
       <Link href="/about">About</Link>
       <Link href="/contact">Contact</Link>
